579de99c0102668399063304abe063aa04639e47
archylex <archylex@protonmail.com>
2020-11-08 23:01:13 +0300

    fix: node

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
+
+app.use(express.bodyParser());
+app.use(express.urlencoded());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname,'views') + '/hello.html');
