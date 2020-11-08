e7895c29044cea16e47b17f965bf66bb203e14fd
archylex <archylex@protonmail.com>
2020-11-08 23:22:33 +0300

    fix: node

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
-const path = require('path');
 const port = process.env.PORT || 3000;
 
-app.use(express.bodyParser());
 app.use(express.urlencoded());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    app.use(express.static(__dirname + '/views'));
+    res.write('Hello!');
+    res.end();
 });
 app.listen(port, () => console.log(`Start listening on port ${port}!`));
